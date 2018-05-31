de30747c5afa2ea2c3aa4c5f057117818dd2a8a8
sitylik <37453609+sitylik@users.noreply.github.com>
2018-06-01 01:32:31 +0500

    hash tabl

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        public class HashTable
+        {
+            static void Main(string[] args)
+            {
+            }
+            class Pair
+            {
+                public object Key { get; set; }
+                public object Value { get; set; }
+            }
+            List<List<Pair>> list;
+            public HashTable(int size)
+            {
+                list = new List<List<Pair>>();
+                for (int i = 0; i < size; i++)
+                {
+                    list.Add(new List<Pair>());
+                }
+            }
+            public void PutPair(object key, object value)
+            {
+                var bucketNumber = GetBucketNumber(key);
+                foreach (var el in list[bucketNumber])
+                {
+                    if (Equals(el.Key, key))
+                    {
+                        el.Value = value;
+                        return;
+                    }
+                }
+                list[bucketNumber].Add(new Pair { Key = key, Value = value });
+            }
+            public object GetValueByKey(object key)
+            {
+                var bucketNumber = GetBucketNumber(key);
+                foreach (var el in list[bucketNumber])
+                {
+                    if (Equals(el.Key, key))
+                    {
+                        return el.Value;
+                    }
+                }
+                return null;
+            }
+            int GetBucketNumber(object key)
+            {
+                return Math.Abs(key.GetHashCode()) % list.Count;
+            }
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestHashTable
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestThreeElements()
+        {
+            string[] value = new string[] { "Компьютер", "Чайник", "Микроволновка" };
+            var table = new HashTable.HashTable(3);
+            for (int i = 0; i < value.Length; i++)
+                table.PutPair(i + 1, value[i]);
+            for (int i = 0; i < value.Length; i++)
+                Assert.AreEqual(table.GetValueByKey(i + 1), value[i]);
+        }
+        [TestMethod]
+        public void TestEqualElements()
+        {
+            string[] value = new string[] { "Чайник", "Микроволновка" };
+            var table = new HashTable.HashTable(3);
+            table.PutPair(1, value[0]);
+            table.PutPair(1, value[1]);
+            Assert.AreEqual(table.GetValueByKey(1), value[0]);
+            Assert.AreEqual(table.GetValueByKey(1), value[1]);
+        }
+        [TestMethod]
+        public void TestManyElements()
+        {
+            int size = 100000;
+            var table = new HashTable.HashTable(size);
+            for (int i = 0; i < size; i++)
+                table.PutPair(i, i++);
+            Assert.AreEqual(table.GetValueByKey(920), 921);
+        }
+        [TestMethod]
+        public void TestManySearchElements()
+        {
+            int size = 100000;
+            var table = new HashTable.HashTable(size);
+            for (int i = 0; i < size; i++)
+                table.PutPair(i, i++);
+            for (int i = 0; i < size; i++)
+                Assert.AreEqual(table.GetValueByKey(1), i + 1);
+        }
+    }
+}
